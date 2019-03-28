b00dc7ad944e6057d24ab885c03c6ca0e97a83f0
Aaron Holbrook <aaron@zeek.com>
2019-03-28 06:24:17 -0500

    Update autoload function to check for directory, rearrange some checks, throw more exceptions on invalid, troublesome types of files for better clarity

--- src/autoload.php
+++ src/autoload.php
@@ -11,16 +11,13 @@ namespace A7;
  */
 function autoload( $directory ) {
 
-	// Get a listing of the current directory
-	$scanned_dir = scandir( $directory );
-
-	if ( empty( $scanned_dir ) ) {
+	// Ensure this path exists
+	if ( ! is_dir( $directory ) ) {
 		return;
 	}
 
-	if ( count( $scanned_dir ) > 200 ) {
-		throw new \Exception( 'Too many files attempted to load via autoload' );
-	}
+	// Get a listing of the current directory
+	$scanned_dir = scandir( $directory );
 
 	// Ignore these items from scandir
 	$ignore = [
@@ -30,6 +27,14 @@ function autoload( $directory ) {
 
 	// Remove the ignored items
 	$scanned_dir = array_diff( $scanned_dir, $ignore );
+
+	if ( empty( $scanned_dir ) ) {
+		return;
+	}
+
+	if ( count( $scanned_dir ) > 250 ) {
+		throw new \Exception( 'Too many files attempted to load via autoload' );
+	}
 
 	foreach ( $scanned_dir as $item ) {
 
@@ -50,24 +55,20 @@ function autoload( $directory ) {
 		if ( 'dir' === $filetype ) {
 
 			autoload( $real_path );
-		}
-
-		// If it's a file, let's try to load it
+		} // If it's a file, let's try to load it
 		else if ( 'file' === $filetype ) {
+
+			if ( true !== is_readable( $real_path ) ) {
+				continue;
+			}
 
 			// Don't allow files that have been uploaded
 			if ( is_uploaded_file( $real_path ) ) {
 				continue;
 			}
 
-			$filesize = filesize( $real_path );
-			// Don't include empty or negative sized files
-			if ( $filesize <= 0 ) {
-				continue;
-			}
-
-			// Don't include files that are greater than 300kb
-			if ( $filesize > 300000 ) {
+			// Only for files that really exist
+			if ( true !== file_exists( $real_path ) ) {
 				continue;
 			}
 
@@ -88,13 +89,16 @@ function autoload( $directory ) {
 				continue;
 			}
 
-			// Only for files that really exist
-			if ( true !== file_exists( $real_path ) ) {
-				continue;
+			$filesize = filesize( $real_path );
+
+			// Don't include negative sized files
+			if ( $filesize < 0 ) {
+				throw new \Exception( 'File size is negative, not autoloading' );
 			}
 
-			if ( true !== is_readable( $real_path ) ) {
-				continue;
+			// Don't include files that are greater than 300kb
+			if ( $filesize > 300000 ) {
+				throw new \Exception( 'File size is greater than 300kb, not autoloading' );
 			}
 
 			require_once( $real_path );
